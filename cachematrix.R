0131e928a27464ae4aa802d09d08e27ef0b95fd6
unknown <wangbo2016@yeah.net>
2014-08-22 21:35:18 +0800

    delete haha.md and modify cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x =matrix()) {
        getinverse = getinverse)
 }
 
-## cacheSolve: This function computes the inverse of the special "matrix" 
+##cacheSolve: This function computes the inverse of the special "matrix" 
 ##returned by makeCacheMatrix above. If the inverse has already been
 ##calculated (and the matrix has not changed), then the cachesolve should 
 ##retrieve the inverse from the cache.
